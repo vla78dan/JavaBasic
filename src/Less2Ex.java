3c92851b8d91e25fdd6700e62915241d4031d6fa
@phpsturm <vla78dan@gmail.com>
2025-03-11 00:01:55 +0300

    less04

---
+++ src/Less2Ex.java
@@ -0,0 +1,23 @@
+public class Less2Ex {
+    public static void main(String[] args) {
+        int tenp;
+        tenp = 10000;
+        System.out.println(tenp);
+
+        int value = 25;
+        value = tenp;
+        System.out.println(value);
+
+        double value2 = 78.123;
+        System.out.println(value2);
+
+        boolean value3 = true;
+        System.out.println(value3);
+
+        char value4 = 'A';
+        System.out.println(value4);
+
+        value4 = 'T';
+        System.out.println(value4);
+    }
+}
