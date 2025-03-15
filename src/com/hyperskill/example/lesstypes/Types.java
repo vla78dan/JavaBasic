f16e661fdd9449b2202e38ad1d64c50e01f5cbbc
@phpsturm <vla78dan@gmail.com>
2025-03-15 13:09:03 +0300

    lessTypes

---
+++ src/com/hyperskill/example/lesstypes/Types.java
@@ -0,0 +1,31 @@
+package com.hyperskill.example.lesstypes;
+
+public class Types {
+
+    public static class Main {
+        public static void main(String[] args) {
+            String haystack = "supercalifragilisticexpialidocious";
+            int haystackLength = 34;
+            String pattern = "cali";
+            int patternLength = 4;
+            boolean next;
+
+            for (int i = 0; i < haystackLength - patternLength + 1; i++) {
+                next = false;
+
+                for (int j = 0; j < patternLength; j++) {
+                    if (haystack.charAt(i + j) != pattern.charAt(j)) {
+                        next = true;
+                        break;
+                    }
+                }
+
+                if (!next) {
+                    System.out.println("Pattern " + pattern + " found in " + haystack);
+                    break;
+                }
+            }
+        }
+    }
+
+}
