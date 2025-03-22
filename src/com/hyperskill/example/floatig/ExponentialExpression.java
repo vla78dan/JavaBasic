e627cd3613c44fd393d43a03d778ab43dd8a98e2
@phpsturm <vla78dan@gmail.com>
2025-03-22 06:28:02 +0300

    ExponentialExpression

---
+++ src/com/hyperskill/example/floatig/ExponentialExpression.java
@@ -0,0 +1,15 @@
+package com.hyperskill.example.floatig;
+
+
+import java.util.Scanner;
+
+public class ExponentialExpression {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double q = sc.nextDouble();
+        double w = Math.pow(q, 3) + Math.pow(q, 2) + q + 1;
+        System.out.println(w);
+        sc.close();
+    }
+}
