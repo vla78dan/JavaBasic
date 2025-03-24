f7dbb7ad2b6c70aeae374ab5923459d34b0358e8
@phpsturm <vla78dan@gmail.com>
2025-03-24 09:27:39 +0300

    Bool

---
+++ src/com/hyperskill/example/dayproblem/WhatsResult.java
@@ -0,0 +1,8 @@
+package com.hyperskill.example.dayproblem;
+
+public class WhatsResult {
+    public static void main(String[] args) {
+        int a = -1;
+        System.out.println(1 - a++);
+    }
+}
