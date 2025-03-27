6820aa93db47d47154c5d911baf6710275b63850
@phpsturm <vla78dan@gmail.com>
2025-03-27 21:40:45 +0300

    DoubleTern_compscience.ch2.less

---
+++ src/com/hyperskill/example/ternary/DoubleTern.java
@@ -0,0 +1,15 @@
+package com.hyperskill.example.ternary;
+
+public class DoubleTern {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = false;
+
+        int result = (b1 && b2) ? 10 : (b2 || b3) ? 20 : 30;
+        System.out.println(result);
+    }
+}
+/*
+20
+ */
