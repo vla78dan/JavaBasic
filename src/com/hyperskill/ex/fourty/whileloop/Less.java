257d6607bb4fe0316971b4a11b53a5d3b3137da2
@phpsturm <vla78dan@gmail.com>
2025-03-31 22:11:28 +0300

    Less_com.hyperskill.ex.fourty.whileloop

---
+++ src/com/hyperskill/ex/fourty/whileloop/Less.java
@@ -0,0 +1,30 @@
+package com.hyperskill.ex.fourty.whileloop;
+
+public class Less {
+    public static void main(String[] args) {
+        /*
+        Print a number 1-10
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+
+         */
+        /*
+        отображает английские буквы в одну строку
+
+        char letter = 'A';
+        while (letter <= 'Z') {
+            System.out.print(letter);
+            letter++;
+
+            */
+        int i = 5;
+        do {
+            i++;
+            System.out.print(i + " ");
+            i -= 2;
+        } while (i > 1);
+    }
+}
+
