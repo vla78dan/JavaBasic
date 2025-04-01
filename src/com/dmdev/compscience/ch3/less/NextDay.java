cbc362098a7af7f5e5d60597db5fcf1b254798bc
@JavaFramer <vla78dan@gmail.com>
2025-04-02 02:21:10 +0300

    NextDay_com.dmdev.compscience.ch3.less

---
+++ src/com/dmdev/compscience/ch3/less/NextDay.java
@@ -0,0 +1,19 @@
+package com.dmdev.compscience.ch3.less;
+
+import java.util.Scanner;
+
+public class NextDay {
+    public static void main(String[] args) {
+        /**
+         * Имеются три числа - день, месяц и год, вводимые пользователем с консоли.
+         * Вывести дату следующего дня в формате "День.Месяц.Год".
+         * Учесть переход на следующий месяц, а также следующий год.
+         * Форматирование строки "День.Месяц.Год" вынести в отдельную функцию.
+         */
+        Scanner sc = new Scanner(System.in);
+
+        int day = sc.nextInt();
+        int month = sc.nextInt();
+        int year = sc.nextInt();
+    }
+}
