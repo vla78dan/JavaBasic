5b4cbd3234ca75351d12fab5d5da56c971de8df4
@phpsturm <vla78dan@gmail.com>
2025-04-01 23:55:01 +0300

    Func18Cube.max_com.dmdev.compscience.ch3.less

---
+++ src/com/dmdev/compscience/ch3/less/Funk182.java
@@ -0,0 +1,12 @@
+package com.dmdev.compscience.ch3.less;
+
+public class Funk182 {
+    public static void main(String[] args) {
+        int value = 5;
+        System.out.println("Cube is: " + cube(value));
+
+    }
+    public static int cube(int value) {
+        return (value * value * value);
+    }
+}
