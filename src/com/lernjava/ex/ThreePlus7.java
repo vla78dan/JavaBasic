6e723b767453a5d552a54d4b9308eaf0bc4efdba
@JavaFramer <vla78dan@gmail.com>
2025-06-02 08:34:08 +0300

    com.zaur.less_l2PrimitiveDataTypes

---
+++ src/com/lernjava/ex/ThreePlus7.java
@@ -0,0 +1,7 @@
+package com.lernjava.ex;
+
+public class ThreePlus7 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/com/zaur/less/l2PrimitiveDataTypes.java
@@ -0,0 +1,18 @@
+package com.zaur.less;
+
+public class l2PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte b1 = 10;
+        byte b2 = 20;
+        byte b3 = 100;
+
+        short s1 = 5;
+        short s2 = -10;
+        short s3 = 0;
+
+        int i1 = 500;
+
+        long l1 = 1000;
+        long l2 = 20000000000L;
+    }
+}
